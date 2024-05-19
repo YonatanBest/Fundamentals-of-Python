e2d6698cea11f37d72571e90bc25a397b4705367
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2024-05-19 21:22:28 +0300

    Create Even or Odd.py

---
+++ Even or Odd.py
@@ -0,0 +1,5 @@
+num = int(input("Enter the number you wanted to know whether even or odd: "))
+if num % 2 == 0:
+    print("The number", num, "is even.")
+else:
+    print("The number", num, "is odd.")
