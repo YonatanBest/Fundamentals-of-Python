92921fd56824c2be610a6dd1e19d3d221b361f0d
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2024-05-21 23:50:47 +0300

    Create Grade System.py

---
+++ Grade System.py
@@ -0,0 +1,11 @@
+grade = float(input("Enter your grade out of 100: "))
+if 90 <= grade:
+    print("Your grade is A.")
+elif 80 <= grade:
+    print("Your grade is B.")
+elif 70 <= grade:
+    print("Your grade is C.")
+elif 50 <= grade:
+    print("Your grade is D.")
+else:
+    print("Your grade is F")
